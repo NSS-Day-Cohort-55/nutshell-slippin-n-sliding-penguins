3fbba3bf277c1b9fea5643e7382c4ba45a257a2d
JavontaeH <hardeman.javontae@gmail.com>
2022-04-13 15:08:07 -0500

    added dashboard to nav and application views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,31 +1,33 @@
-import React from "react"
-import { Routes, Route, Outlet, Navigate } from "react-router-dom"
-import { Login } from "./auth/Login"
-import { Register } from "./auth/Register"
+import React from "react";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 
-export const ApplicationViews = ({isAuthenticated, setIsAuthenticated}) => {
+export const ApplicationViews = ({ isAuthenticated, setIsAuthenticated }) => {
   const PrivateOutlet = () => {
-		return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-	}
-  
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  };
+
   const setAuthUser = (user) => {
-		sessionStorage.setItem("nutshell_user", JSON.stringify(user))
-		setIsAuthenticated(sessionStorage.getItem("nutshell_user") !== null)
-	}
-  
+    sessionStorage.setItem("nutshell_user", JSON.stringify(user));
+    setIsAuthenticated(sessionStorage.getItem("nutshell_user") !== null);
+  };
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<PrivateOutlet/>} >
-        <Route path="/friends" element={""} />
-        <Route path="/messages" element={""} />
-        <Route path="/tasks" element={""} />
-        <Route path="/events" element={""} />
-      </Route>
+      <Routes>
+        <Route path="/" element={<PrivateOutlet />}>
+          <Route exact path="" element={""} />
+          <Route path="articles" element={""} />
+          <Route path="friends" element={""} />
+          <Route path="messages" element={""} />
+          <Route path="tasks" element={""} />
+          <Route path="events" element={""} />
+        </Route>
 
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-    </Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
