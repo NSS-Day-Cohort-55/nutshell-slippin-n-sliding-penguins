19ba30109b8a1883c1ff5feb9d235af017203216
JavontaeH <hardeman.javontae@gmail.com>
2022-04-14 09:50:29 -0500

    initialized data managers for messages and added helper for date formatting

---
+++ src/components/forum/MessageList.js
@@ -0,0 +1,5 @@
+// Module programmed by Jae, should create a list of messages by passing information to message card.
+import React, { useState, useEffect } from "react";
+import * as Fetch from "../../modules/forumDataManager.js";
+
+export const MessageList = () => {};
