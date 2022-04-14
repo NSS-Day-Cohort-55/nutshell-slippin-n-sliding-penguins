f58d9ec007c754c82f7e457c0fc291dbb2ecb5cc
MasterBlaster3 <josephmaifeld@gmail.com>
2022-04-14 11:25:02 -0500

    created taskDataManager, working on TaskDetails

--- src/components/tasks/TaskForm.js
+++ src/components/tasks/TaskForm.js
@@ -1,6 +1,6 @@
 //Author: Joe Maifeld. Component actions:
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TaskForm.css'
 
@@ -8,15 +8,25 @@ export const TaskForm = () => {
   const [] = useState({
     userId: 0,
     taskDescription: "",
-    isComplete: "",
+    isComplete: false,
     timeStamp: "",
   });
 
-  const handleClickSaveTask = (event) => {
+  const navigate = useNavigate()
 
-    const userId = user.userId {			
-			addTask(task).then(() => navigate("/tasks"))
-		}
+  const handleControlledInputChange = (event) => {
+    const newTask = { tasks }
+    let selectedVal = event.target.value
+
+    newTask[event.target.id] = selectedVal
+      setTasks(newTask)
+   }
+
+  const handleClickSaveTask = (event) => {
+     	event.preventDefault()		
+			addTasks(tasks)
+        .then(() => navigate("/tasks"))
+		
 	}
 
   return (
@@ -25,14 +35,14 @@ export const TaskForm = () => {
       <fieldset>
         <div className="form-group">
           <label htmlFor="name">Task Name:</label>
-          <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Task name" value={task.name} />
+          <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="taskName" value={tasks.name} />
         </div>
       </fieldset>
 
       <fieldset>
         <div className="form-group">
 					<label htmlFor="date">Completion Date:</label>
-					<input type="date" id="date" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Task date" value={task.date} />
+					<input type="date" id="date" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Task date" value={tasks.dateDue} />
 				</div>
       </fieldset>
       
--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,1 +1,6 @@
 //Author: Joe Maifeld. Component actions:
+
+import { useEffect, useState } from "react";
+import { getTasks, deleteTask } from "../../modules/TaskDataManager"
+import { TaskCard } from "./Task.js"
+
