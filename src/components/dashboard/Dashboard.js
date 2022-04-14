193f5cbf040fd752632ede54d1fb41226639efce
rushayhays <rushayhays@outlook.com>
2022-04-14 11:03:34 -0500

    articles now appear on the dom

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
+//THis will structure the dashboard and serve as a staging area
+
 import "./Dashboard.css"
+import { ArticleList } from "./articles/ArticleList"
 
 export const Dashboard = () => {
+
 
     return (
         <>
@@ -9,7 +13,7 @@ export const Dashboard = () => {
             </div>
             <div className="dash_main">
                 <div className="dash_events">events go here</div>
-                <div className="dash_news">News goes here</div>
+                <div className="dash_news">< ArticleList/></div>
                 <div className="dash_friends">friends go here</div>                         
             </div>
         </>
