b91aa9ff519e3e994dbb9f282c39f4d549535869
JavontaeH <hardeman.javontae@gmail.com>
2022-04-19 10:12:52 -0500

    added sort to article call

--- src/modules/articleManager.js
+++ src/modules/articleManager.js
@@ -1,48 +1,46 @@
 //This will manage all of the fetch calls for Articles
 //Rushay
 
-const remoteURL = "http://localhost:8088"
+const remoteURL = "http://localhost:8088";
 
 export const getAllArticles = () => {
-    return fetch(`${remoteURL}/articles`)
-    .then(res => res.json())
-}
+  return fetch(`${remoteURL}/articles?_sort=timestamp&_order=desc`).then(
+    (res) => res.json()
+  );
+};
 
 // http://localhost:8088/articles?userId=2
 
 export const getArticlesByUser = (num) => {
-    return fetch(`${remoteURL}/articles?userId=${num}`)
-    .then(res => res.json())
-}
+  return fetch(`${remoteURL}/articles?userId=${num}`).then((res) => res.json());
+};
 
 export const getSpecificArticle = (num) => {
-    return fetch(`${remoteURL}/articles/${num}`)
-    .then(res => res.json())
-}
+  return fetch(`${remoteURL}/articles/${num}`).then((res) => res.json());
+};
 
-  
-export const addArticle = newArticle => {
+export const addArticle = (newArticle) => {
   return fetch(`${remoteURL}/articles`, {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newArticle)
-  }).then(response => response.json())
-}
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newArticle),
+  }).then((response) => response.json());
+};
 
-export const deleteArticle = id => {
-    return fetch(`${remoteURL}/articles/${id}`, {
-      method: "DELETE"
-    }).then(result => result.json())
-}
+export const deleteArticle = (id) => {
+  return fetch(`${remoteURL}/articles/${id}`, {
+    method: "DELETE",
+  }).then((result) => result.json());
+};
 
-export const updateArticle  = (object) => {
-	return fetch(`${remoteURL}/articles/${object.id}`, {
-		method: "PATCH",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify(object)
-	}).then(data => data.json());
-}
+export const updateArticle = (object) => {
+  return fetch(`${remoteURL}/articles/${object.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(object),
+  }).then((data) => data.json());
+};
